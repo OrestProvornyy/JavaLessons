6a89c8a3ae8888e0d66d366d49be722923f6b790
OrestProvornyy <108692461+OrestProvornyy@users.noreply.github.com>
2022-08-07 20:19:48 +0300

    Add files via upload

---
+++ test.java
@@ -0,0 +1,52 @@
+import DriverConfig.BROWSER;
+import DriverConfig.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class test {
+    static final By ELEMENT = By.xpath("//*[@id=\"body\"]/div[1]/main/section[1]/div[2]/div/ul/li[5]/div/div/img");
+    static final String BASEURL="https://ithillel.ua/";
+
+
+
+    public static void main(String[] args) throws Exception {
+
+        WebDriver driver = DriverFactory.getDriver(BROWSER.valueOf("CHROME"));
+        driver.get(BASEURL);
+
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(ELEMENT));
+
+        WebElement element = driver.findElement(ELEMENT);
+        if (element.isDisplayed()) {
+            System.out.println("ELEMENT IS DISPLAYED");
+        } else {
+            System.out.println("Error");
+        }
+
+
+   //     WebElement metaDate = driver.findElement(SEARCH);
+    //    if (!metaDate.getText().equals(text)){
+     //    throw new Exception("Text not equals");
+    //    }
+    //    searchField.sendKeys("JAVA");
+     //   searchField.submit();
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ignore) {
+            driver.quit();
+        }
+
+
+        driver.quit();
+
+
+    }
+}
